ee5e6d4cb8c4be5f1c54149360e1f93c444ba546
Yuvraj Singh Deora <06yuvraj2001singh@gmail.com>
2023-11-10 00:42:49 +0530

    Reupdating Project Files

--- readSerialData.js
+++
@@ -1,6 +0,0 @@
-const { SerialPort } = require('serialport')
-const { ReadlineParser } = require('@serialport/parser-readline')
-const port = new SerialPort({ path: '/dev/cu.usbserial-0001', baudRate: 9600 })
-
-const parser = port.pipe(new ReadlineParser({ delimiter: '\r\n' }))
-parser.on('data', console.log)
